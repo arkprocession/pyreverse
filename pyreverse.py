f1a4cb506faaf58a45120bf5ab5fdb35cf720d74
Nicolas Efler <117755635+arkprocession@users.noreply.github.com>
2023-04-20 18:25:42 -0300

    Add files via upload

--- pyreverse.py
+++ pyreverse.py
@@ -11,6 +11,21 @@ import socket
 import functools
 import requests
 from requests import get
+from cryptography import x509
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.backends import default_backend
+import datetime
+
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
 
 
 class ReverseProxyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
@@ -120,8 +135,52 @@ def run_proxy(host, http_port, https_port):
     if not os.path.exists(cert_file) or not os.path.exists(key_file):
         os.makedirs(cert_dir, exist_ok=True)
 
-        openssl_bin = os.path.join("openssl", "bin", "openssl.exe")
-        os.system(f"{openssl_bin} req -x509 -newkey rsa:2048 -keyout {key_file} -out {cert_file} -days 365 -nodes -subj \"/C=US/ST=California/L=San Francisco/O=MyOrg/OU=MyDept/CN=localhost\"")
+        print("Generating SSL certificate and key...")
+        print(f"Certificate path: {cert_file}")
+        print(f"Key path: {key_file}")
+
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+
+        public_key = private_key.public_key()
+
+        subject = x509.Name([
+            x509.NameAttribute(NameOID.COUNTRY_NAME, "US"),
+            x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "California"),
+            x509.NameAttribute(NameOID.LOCALITY_NAME, "San Francisco"),
+            x509.NameAttribute(NameOID.ORGANIZATION_NAME, "MyOrg"),
+            x509.NameAttribute(NameOID.ORGANIZATIONAL_UNIT_NAME, "MyDept"),
+            x509.NameAttribute(NameOID.COMMON_NAME, "localhost"),
+        ])
+
+        builder = x509.CertificateBuilder().subject_name(
+            subject
+        ).issuer_name(
+            subject
+        ).public_key(
+            public_key
+        ).serial_number(
+            x509.random_serial_number()
+        ).not_valid_before(
+            datetime.datetime.utcnow()
+        ).not_valid_after(
+            datetime.datetime.utcnow() + datetime.timedelta(days=365)
+        ).add_extension(
+            x509.SubjectAlternativeName([x509.DNSName("localhost")]),
+            critical=False,
+        ).sign(private_key, hashes.SHA256(), default_backend())
+
+        with open(cert_file, "wb") as f:
+            f.write(builder.public_bytes(serialization.Encoding.PEM))
+
+        with open(key_file, "wb") as f:
+            f.write(private_key.private_bytes(
+                serialization.Encoding.PEM,
+                serialization.PrivateFormat.TraditionalOpenSSL,
+                serialization.NoEncryption()))
 
     HandlerClass = functools.partial(ReverseProxyHTTPRequestHandler, host, http_port)
     httpd = socketserver.TCPServer(("0.0.0.0", https_port), HandlerClass)  # Bind to all interfaces
@@ -138,8 +197,6 @@ def run_proxy(host, http_port, https_port):
             print(f"- https://{local_ip}:{https_port}")
     print(f"- Public IP: https://{public_ip}:{https_port}")
 
-
-
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
@@ -148,7 +205,9 @@ def run_proxy(host, http_port, https_port):
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
-    config.read("config.ini")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(script_dir, "config.ini")
+    config.read(config_path)
 
     proxy_threads = []
 
